56933c385ee0860c968bcb887b9e76ccdf1911ee
Jonash <jonashtiglao@gmail.com>
2023-12-13 22:22:23 +0800

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { getGenreList, getGamesList } from './api/api'
 
 function App() {
+  const [genreList, setGenreList] = useState(null);
+  
+  useEffect(() => {
+    const fetchGenreList = async () => {
+      try{
+        const {genreList} = await getGenreList();
+        console.log(genreList)
+        setGenreList(genreList);
+      } catch (error) {
+        console.error('Error fetching:', error);
+      }
+    }
+
+    fetchGenreList();
+  }, [])
+  
 
   return (
-    <>
-    </>
+    <div>
+      {genreList && (
+        <ul>
+          {genreList.map((genre) => (
+            <li key={genre.id}>{genre.name}</li>
+          ))}
+        </ul>
+      )}
+    </div>
   )
 }
 
