f15f4e37286dc9965c117814407f574e89d07f7e
Jonash <jonashtiglao@gmail.com>
2023-12-14 16:18:06 +0800

    create home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='bg-[rgb(18,18,18)] w-full min-h-screen py-2'>
+        <div className=' max-w-6xl mx-auto '>
+          <Header />
+        </div>
+    </div>
   )
 }
