dd55ddc211456cc591a10fd6a587fa9067a34b3c
Jonash <jonashtiglao@gmail.com>
2023-12-14 16:15:55 +0800

    added Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Browse from './pages/Browse'
 import { useEffect, useState } from 'react'
-import { getGenreList, getGamesList } from './api/api'
+import { getGenreList, getGamesList, getMostPlayed } from './api/api'
 
 function App() {
-  const [genreList, setGenreList] = useState(null);
+  // const [genreList, setGenreList] = useState(null);
   
-  useEffect(() => {
-    const fetchGenreList = async () => {
-      try{
-        const {genreList} = await getGenreList();
-        console.log(genreList)
-        setGenreList(genreList);
-      } catch (error) {
-        console.error('Error fetching:', error);
-      }
-    }
+  // useEffect(() => {
+  //   const fetchGenreList = async () => {
+  //     try{
+  //       const {upcomingList} = await getMostPlayed();
+  //       console.log(upcomingList)
+  //       // const {genreList} = await getGenreList();
+  //       // console.log(genreList)
+  //       // setGenreList(genreList);
+  //     } catch (error) {
+  //       console.error('Error fetching:', error);
+  //     }
+  //   }
 
-    fetchGenreList();
-  }, [])
+  //   fetchGenreList();
+  // }, [])
   
 
   return (
     <div>
-      {genreList && (
-        <ul>
-          {genreList.map((genre) => (
-            <li key={genre.id}>{genre.name}</li>
-          ))}
-        </ul>
-      )}
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/Browse' element={<Browse />} />
+      </Routes>
     </div>
   )
 }
