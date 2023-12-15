e2c9ddb239f5137f4409e65eb2e381da4edea08e
Jonash <jonashtiglao@gmail.com>
2023-12-15 23:46:33 +0800

    added SwiperCard component

---
+++ src/components/SwiperCard.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react'
+import { IoIosAddCircle  } from "react-icons/io";
+
+
+export default function SwiperCard({game}) {
+    return (
+        <div className='w-[900px] relative rounded-lg' key={game.id}>
+            <img 
+                src={game.background_image} 
+                alt={`${game.name} background`} 
+                className='rounded-2xl opacity-80'
+            />
+            <div className='absolute bottom-10 left-10'>
+                <p className='text-white text-4xl font-semibold w-[300px] pb-11'>{game.name}</p>
+                <button className='bg-white py-5 px-3 rounded-md flex items-center gap-1'>
+                    <IoIosAddCircle  size='1.3rem'/>
+                    <p className='text-xs font-medium'>ADD TO WISHLIST</p>
+                </button>
+            </div>
+        </div>
+    )
+}
