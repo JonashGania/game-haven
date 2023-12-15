579267264aa08e2a9835a5c91629c6bdc289c5c9
Jonash <jonashtiglao@gmail.com>
2023-12-15 23:45:52 +0800

    import HomeSwiper

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../components/Header'
+import HomeSwiper from '../components/HomeSwiper'
 
 export default function Home() {
   return (
     <div className='bg-[rgb(18,18,18)] w-full min-h-screen py-2'>
         <div className=' max-w-6xl mx-auto '>
           <Header />
+          <HomeSwiper />
         </div>
     </div>
   )
