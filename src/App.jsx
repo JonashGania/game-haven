7693866756c433acb157d99e87e27185cd095cc8
Jonash <jonashtiglao@gmail.com>
2023-12-16 23:42:08 +0800

    removed unnecessary lines

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,8 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Browse from './pages/Browse'
-import { useEffect, useState } from 'react'
-import { getGenreList, getGamesList, getMostPlayed, getNewestGames } from './api/api'
 
 function App() {
-  const [genreList, setGenreList] = useState(null);
-  
-  useEffect(() => {
-    const fetchGenreList = async () => {
-      try{
-        const {upcomingList} = await getMostPlayed();
-        console.log(upcomingList)
-        // const {genreList} = await getGenreList();
-        // console.log(genreList)
-        // setGenreList(genreList);
-      } catch (error) {
-        console.error('Error fetching:', error);
-      }
-    }
-
-    fetchGenreList();
-  }, [])
-  
 
   return (
     <div>
