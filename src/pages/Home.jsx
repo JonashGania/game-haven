12f48e27ec6830570caf43d52564c5bb77046f6e
Jonash <jonashtiglao@gmail.com>
2023-12-16 23:45:12 +0800

    removed other imports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Header from '../components/Header'
 import HomeSwiper from '../components/HomeSwiper'
+import StoreTopGames from '../components/StoreTopGames'
 
 export default function Home() {
   return (
-    <div className='bg-[rgb(18,18,18)] w-full min-h-screen py-2'>
-        <div className=' max-w-6xl mx-auto '>
-          <Header />
+    <div className='bg-[rgb(18,18,18)] w-full min-h-screen'>
+        <Header />
+        <div className='max-w-[900px] mx-auto'>
           <HomeSwiper />
+          <StoreTopGames />
         </div>
     </div>
   )
