181047fbbd51bbb2c988c6f52582c3b6eaa06145
Jonash <jonashtiglao@gmail.com>
2023-12-17 23:41:21 +0800

    added loading state

--- src/components/HomeSwiper.jsx
+++ src/components/HomeSwiper.jsx
@@ -3,18 +3,21 @@ import { getNewestGames } from '../api/api'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay } from 'swiper/modules';
 import SwiperCard from './SwiperCard';
+import SkeletonSwiper from './SkeletonSwiper';
 import 'swiper/css';
 import 'swiper/css/autoplay';
 
 
 export default function HomeSwiper() {
     const [newestGamesList, setNewestGamesList] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const fetchGames = async() => {
             try{
                 const { newestGamesList } = await getNewestGames();
                 setNewestGamesList(newestGamesList);
+                setIsLoading(false);
             } catch (error) {
                 console.error('Error fetching games', error);
             }
@@ -26,6 +29,9 @@ export default function HomeSwiper() {
     return (
         <div className='pt-10'>
             <h1 className='w-[900px] mx-auto text-white text-2xl pb-6 font-semibold'>Newest Games</h1>
+            {isLoading && (
+                <SkeletonSwiper />
+            )}
             <Swiper
                 className="mySwiper"
                 modules={[Autoplay]}
