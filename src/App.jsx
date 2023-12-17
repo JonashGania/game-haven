67c479b0cc154c3ff8a20251ae549be7a02a4206
Jonash <jonashtiglao@gmail.com>
2023-12-17 23:40:32 +0800

    added skeletonTheme

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Browse from './pages/Browse'
+import { SkeletonTheme } from 'react-loading-skeleton'
 
 function App() {
 
   return (
-    <div>
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/Browse' element={<Browse />} />
-      </Routes>
-    </div>
+    <>
+      <SkeletonTheme baseColor='#313131' highlightColor='#525252'>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/Browse' element={<Browse />} />
+        </Routes>
+      </SkeletonTheme>
+    </>
   )
 }
 
