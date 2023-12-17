057545a62388d0edbf8b346b7566564850d3e7f1
Jonash <jonashtiglao@gmail.com>
2023-12-17 15:56:50 +0800

    added date format

--- src/utils/formattedDate.js
+++ src/utils/formattedDate.js
@@ -1,10 +1,13 @@
 export function formattedDate(){
     const currentDate = new Date();
     const lastThirtyDays = new Date();
+    const fiveYearsAgo = new Date();
     lastThirtyDays.setDate(lastThirtyDays.getDate() - 30);
+    fiveYearsAgo.setFullYear(fiveYearsAgo.getFullYear() - 3);
 
     const formattedCurrentDate = currentDate.toISOString().split('T')[0];
     const formattedLastThirtyDays = lastThirtyDays.toISOString().split('T')[0];
+    const formattedFiveYearsAgo = fiveYearsAgo.toISOString().split('T')[0];
 
-    return{ formattedCurrentDate, formattedLastThirtyDays}
+    return{ formattedCurrentDate, formattedLastThirtyDays, formattedFiveYearsAgo}
 }
