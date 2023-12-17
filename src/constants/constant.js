69763fcbdaf300f7c0c0762ff403c5564ce71534
Jonash <jonashtiglao@gmail.com>
2023-12-17 15:59:06 +0800

    added constants file

---
+++ src/constants/constant.js
@@ -0,0 +1,23 @@
+export const data = {
+    usefulLink: [
+        {
+            page: 'My Github',
+            link: 'https://github.com/JonashGania',
+        }, {
+            page: 'Rawg API',
+            link: 'https://rawg.io/apidocs',
+        }, {
+            page: 'Steam',
+            link: 'https://store.steampowered.com',
+        }, {
+            page: 'PlayStation Store',
+            link: 'https://store.playstation.com/en-us/pages/latest',
+        }, {
+            page: 'Xbox Store',
+            link: 'https://www.xbox.com/en-us/games?xr=shellnav',
+        }, {
+            page: 'Nintendo Store',
+            link: 'https://www.nintendo.com/us/store/'
+        }
+    ]
+}
