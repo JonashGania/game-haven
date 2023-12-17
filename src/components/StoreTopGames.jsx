b082f65b34af27bf3cf05b3657ae3fa1f7a0f042
Jonash <jonashtiglao@gmail.com>
2023-12-17 15:55:52 +0800

    import components

--- src/components/StoreTopGames.jsx
+++ src/components/StoreTopGames.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import SteamGamesSwiper from './SteamGamesSwiper'
+import PSGamesSwiper from './PSGamesSwiper'
+import XboxGamesSwiper from './XboxGamesSwiper'
 
 export default function StoreTopGames() {
   return (
     <div className='pt-16 pb-36 max-w-[900px] mx-auto'>
-        <h1 className='text-center text-white text-2xl font-medium'>Top Games on Stores!</h1>
-        <SteamGamesSwiper />
+        <h1 className='text-center text-white text-2xl font-medium pb-10'>Top Games on Stores!</h1>
+        <div className='flex flex-col gap-10'>
+          <SteamGamesSwiper />
+          <PSGamesSwiper />
+          <XboxGamesSwiper />
+        </div>
     </div>
   )
 }
