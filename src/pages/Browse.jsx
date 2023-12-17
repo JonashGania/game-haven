d1f92f113900c5deb1c019e464db21c9a2d9da3b
Jonash <jonashtiglao@gmail.com>
2023-12-17 23:44:26 +0800

    import sidebar and wishlist

--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import Header from '../components/Header'
+import Sidebar from '../components/Sidebar'
+import GameLists from '../components/GameLists'
 
 export default function Browse() {
   return (
-    <div>Browse</div>
+    <div className='bg-[rgb(18,18,18)] w-full min-h-screen'>
+      <Header />
+      <div className='max-w-6xl mx-auto pt-8 flex gap-4'>
+        <Sidebar />
+        <GameLists />
+      </div>
+    </div>
   )
 }
