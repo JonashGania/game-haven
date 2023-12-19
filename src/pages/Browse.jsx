b16c852e6a098e4506d9b2f589c886ff85a1416f
Jonash <jonashtiglao@gmail.com>
2023-12-19 15:53:55 +0800

    minor changes

--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/Header'
 import Sidebar from '../components/Sidebar'
 import GameLists from '../components/GameLists'
+import WishList from '../components/WishList'
 
 export default function Browse() {
+
+
   return (
     <div className='bg-[rgb(18,18,18)] w-full min-h-screen'>
       <Header />
-      <div className='max-w-6xl mx-auto pt-8 flex gap-4'>
+      <div className='max-w-6xl mx-auto pt-8 flex gap-4 px-4'>
         <Sidebar />
         <GameLists />
       </div>
+      <WishList />
     </div>
   )
 }
