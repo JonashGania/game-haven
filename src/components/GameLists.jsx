8fa7acde91b1156d25d505c648596cb46833fc10
Jonash <jonashtiglao@gmail.com>
2023-12-19 15:50:56 +0800

    minor changes

--- src/components/GameLists.jsx
+++ src/components/GameLists.jsx
@@ -1,14 +1,21 @@
 import React, {useEffect, useState} from 'react'
-import { getSteamTopGames } from '../api/api'
+import { getSteamTopGames, getGamesList } from '../api/api'
+import Games from './Games';
+import SkeletonGames from './SkeletonGames';
 
 export default function GameLists() {
     const [gameLists, setGameLists] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         async function fetchGameLists(){
             try {
+                setIsLoading(true);
+
                 const { topGames } = await getSteamTopGames();
                 setGameLists(topGames);
+
+                setIsLoading(false);
             } catch (error) {
                 console.error('Error fetching games', error)
             }  
@@ -21,26 +28,16 @@ export default function GameLists() {
         <div className='flex-1'>
             <h1 className='text-white text-4xl font-extrabold pb-4'>Popular Games</h1>
             <ul className='game-grid grid gap-y-8 gap-4'>
-                {gameLists.map((game) => (
-                    <li key={game.id} className='w-full'>
-                        <div className='w-full h-[170px]'>
-                            <img 
-                                src={game.background_image} 
-                                alt={`${game.name} background`} 
-                                className='w-full h-full rounded-md'
-                            />
-                        </div>
-                        <div className='flex justify-between items-center'>
-                            <ul className='flex items-center gap-2 pt-3 pb-2'>
-                                {game.genres.map((genre) => (
-                                    <li key={genre.id} className='text-neutral-400 font-medium text-xs'>{genre.name}</li>
-                                ))}
-                            </ul>
-                            <span className='text-green-400 border border-white px-1 py-1 rounded-sm text-xs'>{game.metacritic}</span>
-                        </div>
-                        <h1 className='text-white'>{game.name}</h1>
-                    </li>
-                ))}
+                {isLoading ? (
+                    Array.from({ length: 20 }).map((_, index) => (
+                        <SkeletonGames key={index}/>
+                    ))
+                ) : (
+                    gameLists.map((game, index) => (
+                        <Games game={game} key={index}/>
+                    ))
+                )}
+
             </ul>
         </div>
     )
