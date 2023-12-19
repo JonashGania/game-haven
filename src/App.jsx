e4215d23c90d171582aff2f6e9d7d60846470122
Jonash <jonashtiglao@gmail.com>
2023-12-19 23:51:25 +0800

    added Browse child route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Browse from './pages/Browse'
+import GenreGames from './components/GenreGames'
+import GameLists from './components/GameLists'
 import { SkeletonTheme } from 'react-loading-skeleton'
 
 function App() {
@@ -10,7 +12,10 @@ function App() {
       <SkeletonTheme baseColor='#313131' highlightColor='#525252'>
         <Routes>
           <Route path='/' element={<Home />}/>
-          <Route path='/Browse' element={<Browse />} />
+          <Route path='/Browse' element={<Browse />} >
+            <Route index element={<GameLists />}/>
+            <Route path=':genreName' element={<GenreGames />}/>
+          </Route>
         </Routes>
       </SkeletonTheme>
     </>
