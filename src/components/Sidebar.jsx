32d841ef3534bf4e194bf95d4d2a787652d84b23
Jonash <jonashtiglao@gmail.com>
2023-12-19 23:53:30 +0800

    added genre navigation

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import { getGenreList } from '../api/api'
 import SkeletonSidebar from './SkeletonSidebar';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function Sidebar() {
   const [genres, setGenres] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const location = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function fetchGenres(){
@@ -20,6 +23,10 @@ export default function Sidebar() {
     fetchGenres();
   }, [])
 
+  const handleGenreNavigate = (genreName) => {
+    navigate(`/Browse/${genreName}`);
+  }
+
   return (
     <div className='scrollbar-hide w-[18%] overflow-y-auto max-h-[82vh] sticky top-[8rem]'>
       <h1 className='text-white font-extrabold text-2xl pb-2 pl-4'>Genres</h1>
@@ -32,7 +39,8 @@ export default function Sidebar() {
           genres.map((genre) => (
             <li 
               key={genre.id} 
-              className='flex items-center gap-3 hover:bg-neutral-600 rounded-lg py-3 px-3 cursor-pointer'
+              className={`flex items-center gap-3 hover:bg-neutral-600 rounded-lg py-3 px-3 cursor-pointer ${location.pathname.includes(genre.slug) ? 'bg-neutral-600' : 'bg-transparent'}`}
+              onClick={() => handleGenreNavigate(genre.slug)}
             >
               <img src={genre.image_background} alt='genre image' className='w-[40px] h-[40px] rounded-lg'/>
               <span className='text-neutral-300'>{genre.name}</span>
