249420e69989fd1296648fafbbf101234c3fe87a
Jonash <jonashtiglao@gmail.com>
2023-12-19 15:54:28 +0800

    created component for games

---
+++ src/components/Games.jsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+import { IoIosAddCircle  } from "react-icons/io";
+
+export default function Games({ game }) {
+    const [isHovered, setIsHovered] = useState(false);
+    return (
+        <li key={game.id} className='w-full'>
+            <div 
+                className='w-full h-[170px] relative group'
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+            >
+                <img 
+                    src={game.background_image} 
+                    alt={`${game.name} background`} 
+                    className='w-full h-full rounded-md group'
+                />
+                <div className='absolute inset-0 w-full h-full rounded-md bg-white bg-opacity-0 group-hover:bg-opacity-10 transition-all duration-100 ease-in'></div>
+                {isHovered && (
+                    <button  className='absolute top-3 right-3 bg-white rounded-[50%]'>
+                        <IoIosAddCircle size='1.3rem'/>
+                    </button>
+                )}
+            </div>
+            <div className='flex justify-between items-center'>
+                <ul className='flex items-center gap-2 pt-3 pb-2'>
+                    {game.genres.map((genre) => (
+                        <li key={genre.id} className='text-neutral-400 font-medium text-xs'>{genre.name}</li>
+                    ))}
+                </ul>
+                <span className='text-green-400 border border-neutral-500 px-1 py-1 rounded-sm text-xs'>{game.metacritic}</span>
+            </div>
+            <h1 className='text-white'>{game.name}</h1>
+        </li>
+    )
+}
