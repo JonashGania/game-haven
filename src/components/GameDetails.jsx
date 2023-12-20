dcad0cdcda2751080bb51835b6c18de1f73f5358
Jonash <jonashtiglao@gmail.com>
2023-12-20 15:49:15 +0800

    added more game details

--- src/components/GameDetails.jsx
+++ src/components/GameDetails.jsx
@@ -1,17 +1,22 @@
 import React, {useEffect, useState} from 'react'
 import { getGameDetails } from '../api/api'
+import { IoIosAddCircle  } from "react-icons/io";
+import { cutParagraph } from '../utils/cutParagraph';
 import StarRatings from 'react-star-ratings';
 
 
 export default function GameDetails() {
     const [game, setGame] = useState({});
+    const [paragraph, setParagraph] = useState(null);
+    const [screenshots, setScreenshots] = useState([]);
 
     useEffect(() => {
         async function fetchGameDetails() {
             try {
-                const { results } = await getGameDetails();
+                const { results, screenshots } = await getGameDetails();
                 setGame(results)
-                console.log(results)
+                setParagraph(cutParagraph(results.description_raw))
+                setScreenshots(screenshots)
             } catch (error) {
                 console.error('Error fetching data', error);
             }
@@ -38,25 +43,33 @@ export default function GameDetails() {
                     </div>
                     <div className='flex gap-8'>
                         <div className='w-[650px]'>
-                            <p className='text-neutral-200 text-sm'>{game.description_raw}</p>
+                            <p className='text-neutral-200 text-sm'>{paragraph}</p>
                         </div>
                         <div className='flex-1 px-8 flex flex-col gap-3'>
                             <div className='w-full flex items-center justify-between gap-4 pb-1 border-b border-neutral-800'>
                                 <span className='text-neutral-400'>Developer</span>
-                                {game.developers.map((name) => (
-                                    <h3 className='text-neutral-200' key={name.id}>{name.name}</h3>
+                                {game.developers?.slice(0,1).map((name) => (
+                                    <h3 className='text-neutral-200 text-sm' key={name.id}>{name.name}</h3>
                                 ))}
                             </div>
                             <div className='w-full flex items-center justify-between gap-4 pb-1 border-b border-neutral-800'>
                                 <span className='text-neutral-400'>Publisher</span>
-                                {game.publishers.map((name) => (
-                                    <h3 className='text-neutral-200' key={name.id}>{name.name}</h3>
+                                {game.publishers?.map((name) => (
+                                    <h3 className='text-neutral-200 text-sm' key={name.id}>{name.name}</h3>
                                 ))}
                             </div>
                             <div className='w-full flex items-center justify-between gap-4 pb-1 border-b border-neutral-800'>
                                 <span className='text-neutral-400'>Released</span>
-                                <h3 className='text-neutral-200'>{game.released}</h3>
+                                <h3 className='text-neutral-200 text-sm'>{game.released}</h3>
                             </div>
+                            <div className='w-full flex items-center justify-between gap-4 pb-1 border-b border-neutral-800'>
+                                <span className='text-neutral-400'>Metacritic</span>
+                                <h3 className='text-neutral-200 text-sm'>{game.metacritic}</h3>
+                            </div>
+                            <button className='py-1 flex items-center gap-2 justify-center border border-neutral-200 rounded-sm hover:bg-[rgba(255,255,255,0.2)]'>
+                                <IoIosAddCircle size='1.3rem' color='white'/>
+                                <p className='text-xs uppercase text-neutral-200 font-medium'>Add to wishlist</p>
+                            </button>
                         </div>
                     </div>
                 </div>
