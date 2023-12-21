c02a9fa88e469a67c924d8eb29cf8da8d5ae396e
Jonash <jonashtiglao@gmail.com>
2023-12-21 23:30:06 +0800

    add to wishlist function

--- src/components/GameSlides.jsx
+++ src/components/GameSlides.jsx
@@ -1,24 +1,40 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 import { IoIosAddCircle  } from "react-icons/io";
+import { useWishlist } from '../context/WishlistContext';
 
 export default function GameSlides({game}) {
     const [isHovered, setIsHovered] = useState(false);
+    const { addToWishlist } = useWishlist();
+    const primaryGenre = game.genres[0].slug;
+
+    const handleAddToWishlist = (game) => {
+        addToWishlist(game)
+    }
 
     return (
-        <li className='w-[290px]' key={game.id}>
+        <li 
+            className='w-[290px]' 
+            key={game.id}>
             <div 
                 className='h-[157px] w-full relative group'
                 onMouseEnter={() => setIsHovered(true)}
                 onMouseLeave={() => setIsHovered(false)}
             >
-                <img 
-                    src={game.background_image}
-                     alt={`${game.name} image`} 
-                    className='w-full h-full rounded-md group' 
-                />
-                <div className='absolute inset-0 w-full h-full rounded-md bg-white bg-opacity-0 group-hover:bg-opacity-10 transition-all duration-100 ease-in'></div>
+                <Link to={`/browse/${primaryGenre}/${game.id}`}>
+                    <img 
+                        src={game.background_image}
+                        alt={`${game.name} image`} 
+                        className='w-full h-full rounded-md group' 
+                    />
+                    <div className='absolute inset-0 w-full h-full rounded-md bg-white bg-opacity-0 group-hover:bg-opacity-10 transition-all duration-100 ease-in'></div>
+                </Link>
+                
                 {isHovered && (
-                    <button className='absolute top-3 right-3 bg-white rounded-[50%]'>
+                    <button 
+                        className='absolute top-3 right-3 bg-white rounded-[50%]'
+                        onClick={() => handleAddToWishlist(game)}
+                    >
                         <IoIosAddCircle size='1.3rem'/>
                     </button>
                 )}
