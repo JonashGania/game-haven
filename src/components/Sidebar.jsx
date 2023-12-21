b8aea546b618c37c9ccbbe3f823cda6da53993e3
Jonash <jonashtiglao@gmail.com>
2023-12-21 23:31:01 +0800

    change url params

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -24,7 +24,7 @@ export default function Sidebar() {
   }, [])
 
   const handleGenreNavigate = (genreName) => {
-    navigate(`/Browse/${genreName}`);
+    navigate(`/browse/${genreName}`);
   }
 
   return (
