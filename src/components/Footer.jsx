caa3dbb5bde43bd5968c89688288cbdf8f4c38a1
Jonash <jonashtiglao@gmail.com>
2023-12-23 22:50:17 +0800

    fix responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,16 +4,16 @@ import { data } from '../constants/constant'
 export default function Footer() {
   return (
     <footer className='w-full min-h-[100px] bg-[rgb(34,34,34)]'>
-        <div className='max-w-6xl mx-auto py-10 flex flex-wrap gap-4 items-center justify-center md:justify-between'>
+        <div className='max-w-6xl mx-auto py-10 flex flex-wrap gap-4 items-center justify-center tablet:justify-between'>
             <div className='px-4'>
                 <img src="logo.svg" alt="logo" />
-                <p className='w-[450px] text-neutral-300 font-light text-sm pl-3'>This project is not a real Game store, Game Haven is just personal project of mine. 
+                <p className='max-w-[450px] text-neutral-300 font-light text-sm pl-3'>This project is not a real Game store, Game Haven is just personal project of mine. 
                     All the games in here are not for sale. This project just simply display games on different genres using Rawg API. Enjoy!
                 </p>
             </div>
             <div className='px-8'>
                 <h3 className='text-white font-medium pb-2 text-lg text-center'>Useful Links</h3>
-                <ul className='grid flex-col gap-3 grid-cols-2'>
+                <ul className='grid mobile:gap-3 gap-1 grid-cols-2'>
                     {data.usefulLink.map((page, index) => (
                         <li  key={index}>
                             <a href={page.link} target='_blank' rel='noreferrer' className='text-sm text-neutral-300 hover:text-white'>{page.page}</a>
