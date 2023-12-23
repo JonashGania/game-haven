59f24215f5c9a64809365922ab4d18011b368699
Jonash <jonashtiglao@gmail.com>
2023-12-23 16:40:01 +0800

    fix mobile view

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,9 +8,9 @@ import XboxGamesSwiper from './Swiper/XboxGamesSwiper'
 
 export default function Home() {
   return (
-    <div className='bg-[rgb(18,18,18)] w-full min-h-screen'>
+    <div className='bg-[rgb(18,18,18)] w-full min-h-screen overflow-hidden'>
         <Header />
-        <div className='max-w-[900px] mx-auto pb-32'>
+        <div className='max-w-[900px] mx-auto pb-32 px-4'>
           <HomeSwiper />
           <h1 className='text-center text-white text-2xl font-medium pt-20 pb-10'>Top Games on Stores!</h1>
           <div className='flex flex-col gap-10'>
