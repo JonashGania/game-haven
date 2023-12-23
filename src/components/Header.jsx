0e8f6fcab008c948a2d01d48665ea9015e6b80b4
Jonash <jonashtiglao@gmail.com>
2023-12-23 16:38:25 +0800

    fix mobile view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import { Link, NavLink } from 'react-router-dom'
 import SearchGames from './SearchBar'
 import WishList from './WishList';
 import { MdOutlineAddBox } from "react-icons/md";
+import { IoIosSearch } from "react-icons/io";
 
 export default function Header() {
     const [openWishlist, setOpenWishlist] = useState(false);
@@ -17,15 +18,15 @@ export default function Header() {
 
   return (
     <header className='sticky top-0 z-20 h-24 bg-[rgb(18,18,18)] w-full'>
-        <nav className='flex items-center justify-between py-4 max-w-6xl mx-auto px-4'>
+        <nav className='flex items-center justify-between gap-4 py-4 max-w-6xl mx-auto px-4'>
             <div>
                 <Link to='/'>
                     <img src="/logo.svg" alt="avatar"/>
                 </Link>
             </div>
             <SearchGames />
-            <div className='flex items-center gap-5'>
-                <div className='flex items-center gap-9'>
+            <div className='flex items-center gap-3 tablet:gap-5'>
+                <div className='hidden tablet:flex items-center gap-9'>
                     <NavLink to='/' className='text-neutral-300'>
                         Home
                     </NavLink>
@@ -33,6 +34,12 @@ export default function Header() {
                         Browse
                     </NavLink>
                 </div>
+                <button className='block tablet:hidden'>
+                    <IoIosSearch 
+                        color='rgb(212,212,212)' 
+                        size='1.5rem'
+                    />
+                </button>
                 <div className='w-[2px] bg-neutral-300 h-6'></div>
                 <button onClick={handleOpenWishlist}>
                     <MdOutlineAddBox color='rgb(212,212,212)' size='1.8rem'/>
