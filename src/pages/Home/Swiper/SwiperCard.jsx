efd46550fb4382621bbee451cf0a5d7b2485f0af
Jonash <jonashtiglao@gmail.com>
2023-12-23 22:52:16 +0800

    fix responsive

--- src/pages/Home/Swiper/SwiperCard.jsx
+++ src/pages/Home/Swiper/SwiperCard.jsx
@@ -27,8 +27,8 @@ export default function SwiperCard({game}) {
                     className='rounded-2xl opacity-80'
                 />
             </Link>
-            <div className='absolute bottom-10 left-10'>
-                <p className='text-white text-xl tablet:text-4xl font-semibold w-[300px] pb-3 tablet:pb-11 transition-all duration-300 ease-in'>{game.name}</p>
+            <div className='absolute mobile:bottom-10 bottom-5 mobile:left-10 left-5 transition-all duration-200 ease-in'>
+                <p className='text-white text-lg tablet:text-4xl font-semibold mobile:w-[300px] w-[200px] pb-3 tablet:pb-11 transition-all duration-300 ease-in'>{game.name}</p>
                 <button 
                     className='bg-white transition-all duration-200 ease-in py-3 tablet:py-5 px-3 tablet:px-3 rounded-md flex items-center gap-2'
                     onClick={() => handleToggleWishlist(game)}
