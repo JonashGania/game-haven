8ac22a5064beb75e411252c04f359ca366122078
Jonash <jonashtiglao@gmail.com>
2023-12-23 22:51:48 +0800

    fix responsive

--- src/pages/Home/Skeleton/SkeletonSlides.jsx
+++ src/pages/Home/Skeleton/SkeletonSlides.jsx
@@ -4,8 +4,18 @@ import 'react-loading-skeleton/dist/skeleton.css'
 
 export default function SkeletonSlides() {
   return (
-    <div className='w-[290px]'>
-        <Skeleton count={1} height='157px' width='290px' className='rounded-md'/>
+    <>
+      <div className='mobile:block hidden'>
+          <Skeleton count={1} height='157px' width='100%' className='rounded-md'/>
+          <div className='flex items-center gap-4'>
+              <Skeleton count={1} height='10px' width='30px'/>
+              <Skeleton count={1} height='10px' width='30px'/>
+              <Skeleton count={1} height='10px' width='30px'/>
+          </div>
+          <Skeleton count={1} height='10px' width='100px'/>
+      </div>
+      <div className='mobile:hidden block'>
+        <Skeleton count={1} height='180px' width='100%' className='rounded-md'/>
         <div className='flex items-center gap-4'>
             <Skeleton count={1} height='10px' width='30px'/>
             <Skeleton count={1} height='10px' width='30px'/>
@@ -13,5 +23,6 @@ export default function SkeletonSlides() {
         </div>
         <Skeleton count={1} height='10px' width='100px'/>
     </div>
+    </>
   )
 }
