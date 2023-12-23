d8829d69dfa853f156a64ddec2f1fd03e78990ce
Jonash <jonashtiglao@gmail.com>
2023-12-23 22:51:59 +0800

    fix responsive

--- src/pages/Home/Skeleton/SkeletonSwiper.jsx
+++ src/pages/Home/Skeleton/SkeletonSwiper.jsx
@@ -4,8 +4,16 @@ import 'react-loading-skeleton/dist/skeleton.css'
 
 export default function SkeletonSwiper() {
   return (
-    <div>
-        <Skeleton count={1} height='500px' width='888.88px' className='rounded-2xl'/>
-    </div>
+    <>    
+      <div className='hidden sidebar:block px-4'>
+        <Skeleton count={1} height='500px'  width='100%' className='rounded-2xl'/>
+      </div>
+      <div className='hidden mobile:block sidebar:hidden px-4'>
+        <Skeleton count={1} height='350px'  width='100%' className='rounded-2xl'/>
+      </div>
+      <div className='bloc mobile:hidden'>
+        <Skeleton count={1} height='170px'  width='100%' className='rounded-2xl'/>
+      </div>
+    </>
   )
 }
