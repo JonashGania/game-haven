9e879ece0a3a4e6d2149ceb33da2c3c0d2bf952a
Jonash <jonashtiglao@gmail.com>
2023-12-27 01:42:02 +0800

    navigate link games

--- src/components/SearchFilterResults.jsx
+++ src/components/SearchFilterResults.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
 export default function SearchFilterResults({ results }) {
   return (
-    <div className='scrollbar-hide absolute bg-[rgb(32,32,32)] w-full h-[200px] overflow-y-auto py-4 px-4 top-14 rounded-md'>
+    <div className='scrollbar-hide absolute bg-[rgb(32,32,32)] w-full h-[250px] overflow-y-auto px-4 top-14 rounded-md'>
         <ul className='flex flex-col gap-2'>
             {results.map((game) => (
-                <li key={game.id} className='text-white flex items-center gap-6 hover:bg-neutral-700 py-2 px-2 rounded-md'>
+                <Link 
+                  to={game.genres && game.genres.length > 0 ? `/browse/${game.genres[0].slug}/${game.id}` : '#'}
+                  key={game.id} 
+                  className=' flex items-center gap-6 cursor-pointer hover:bg-neutral-700 py-2 px-2 rounded-md'
+                >
                     <img src={game.background_image} alt='image' className='w-[60px] h-[40px] rounded-md'/>
                     <h3 className='text-neutral-200'>{game.name}</h3>
-                </li>
+                </Link>
             ))}
         </ul>
     </div>
