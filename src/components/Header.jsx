87d1635b67ad2a376772ed250cded24f160f0a3d
Jonash <jonashtiglao@gmail.com>
2023-12-26 22:38:05 +0800

    added search button close/open func

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,17 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
+import { MdOutlineAddBox } from "react-icons/md";
+import { IoIosSearch } from "react-icons/io";
 import SearchGames from './SearchBar'
 import WishList from './WishList';
 import Dropdown from './Dropdown';
-import { MdOutlineAddBox } from "react-icons/md";
-import { IoIosSearch } from "react-icons/io";
+import MobileSearchGames from './MobileSearchGames';
+
 
 
 export default function Header() {
     const [openWishlist, setOpenWishlist] = useState(false);
+    const [openSearch, setOpenSearch] = useState(false);
 
     const handleOpenWishlist = () => {
         setOpenWishlist(true);
@@ -18,40 +21,52 @@ export default function Header() {
         setOpenWishlist(false);
     }
 
-  return (
-    <header className='sticky top-0 z-20 h-24 bg-[rgb(18,18,18)] w-full'>
-        <nav className='flex items-center justify-between gap-4 py-4 max-w-6xl mx-auto px-4'>
-            <div>
-                <Link to='/'>
-                    <img src="/logo.svg" alt="avatar"/>
-                </Link>
-            </div>
-            <SearchGames />
-            <div className='block tablet:hidden'>
-                <Dropdown />
-            </div>
-            <div className='flex items-center gap-3 tablet:gap-5'>
-                <div className='hidden tablet:flex items-center gap-9'>
-                    <NavLink to='/' className='text-neutral-300'>
-                        Home
-                    </NavLink>
-                    <NavLink to='/browse' className='text-neutral-300'>
-                        Browse
-                    </NavLink>
+    const handleOpenSearch = () => {
+        setOpenSearch(true)
+    }
+
+    const handleCloseSearch = () => {
+        setOpenSearch(false)
+    }
+
+    return (
+        <header className='sticky top-0 z-20 h-24 bg-[rgb(18,18,18)] w-full'>
+            <nav className='flex items-center justify-between gap-4 py-4 max-w-6xl mx-auto px-4'>
+                <div>
+                    <Link to='/'>
+                        <img src="/logo.svg" alt="avatar"/>
+                    </Link>
                 </div>
-                <button className='block tablet:hidden'>
-                    <IoIosSearch 
-                        color='rgb(212,212,212)' 
-                        size='1.5rem'
-                    />
-                </button>
-                <div className='w-[2px] bg-neutral-300 h-6'></div>
-                <button onClick={handleOpenWishlist}>
-                    <MdOutlineAddBox color='rgb(212,212,212)' size='1.8rem'/>
-                </button>
-            </div>
-        </nav>
-        <WishList isOpen={openWishlist} onClose={handleCloseWishlist}/>
-    </header>
-  )
+                <SearchGames />
+                <div className='block tablet:hidden'>
+                    <Dropdown />
+                </div>
+                <div className='flex items-center gap-3 tablet:gap-5'>
+                    <div className='hidden tablet:flex items-center gap-9'>
+                        <NavLink to='/' className='text-neutral-300'>
+                            Home
+                        </NavLink>
+                        <NavLink to='/browse' className='text-neutral-300'>
+                            Browse
+                        </NavLink>
+                    </div>
+                    <button 
+                        className='block tablet:hidden'
+                        onClick={handleOpenSearch}
+                    >
+                        <IoIosSearch 
+                            color='rgb(212,212,212)' 
+                            size='1.5rem'
+                        />
+                    </button>
+                    <div className='w-[2px] bg-neutral-300 h-6'></div>
+                    <button onClick={handleOpenWishlist}>
+                        <MdOutlineAddBox color='rgb(212,212,212)' size='1.8rem'/>
+                    </button>
+                </div>
+            </nav>
+            <WishList isOpen={openWishlist} onClose={handleCloseWishlist}/>
+            {openSearch && <MobileSearchGames handleCloseSearch={handleCloseSearch}/>}
+        </header>
+    )
 }
