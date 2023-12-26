c3857c85af2790edabb7497734e163454963f1b1
Jonash <jonashtiglao@gmail.com>
2023-12-26 16:00:45 +0800

    import filterGenre

--- src/pages/Browse/Games/GameLists.jsx
+++ src/pages/Browse/Games/GameLists.jsx
@@ -2,6 +2,7 @@ import {useEffect, useState} from 'react'
 import { getSteamTopGames } from '../../../api/api'
 import Games from './Games';
 import SkeletonGames from '../Skeleton/SkeletonGames';
+import FilterGenre from '../../../components/FilterGenre';
 
 export default function GameLists() {
     const [gameLists, setGameLists] = useState([]);
@@ -24,8 +25,11 @@ export default function GameLists() {
     }, [])
 
     return (
-        <div className='flex-1 pb-10 px-8 mobile:px-0 transition-all duration-200 ease-in'>
-            <h1 className='text-white text-4xl font-extrabold pb-4'>Popular Games</h1>
+        <div className='flex-1 pb-10 transition-all duration-200 ease-in'>
+            <div className='block sidebar:hidden pb-4'>
+                <FilterGenre />
+            </div>
+            <h1 className='text-white text-xl mobile:text-4xl font-extrabold pb-4'>Popular Games</h1>
             <ul className='game-grid grid gap-y-8 gap-4'>
                 {isLoading ? (
                     Array.from({ length: 20 }).map((_, index) => (
