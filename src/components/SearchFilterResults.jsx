ed6309d841c2c89cf752d9bcc7f11ed6b59dfad4
Jonash <jonashtiglao@gmail.com>
2023-12-27 14:44:24 +0800

    fix layout

--- src/components/SearchFilterResults.jsx
+++ src/components/SearchFilterResults.jsx
@@ -2,19 +2,17 @@ import { Link } from "react-router-dom"
 
 export default function SearchFilterResults({ results }) {
   return (
-    <div className='scrollbar-hide absolute bg-[rgb(32,32,32)] w-full h-[250px] overflow-y-auto px-4 top-14 rounded-md'>
-        <ul className='flex flex-col gap-2'>
-            {results.map((game) => (
-                <Link 
-                  to={game.genres && game.genres.length > 0 ? `/browse/${game.genres[0].slug}/${game.id}` : '#'}
-                  key={game.id} 
-                  className=' flex items-center gap-6 cursor-pointer hover:bg-neutral-700 py-2 px-2 rounded-md'
-                >
-                    <img src={game.background_image} alt='image' className='w-[60px] h-[40px] rounded-md'/>
-                    <h3 className='text-neutral-200'>{game.name}</h3>
-                </Link>
-            ))}
-        </ul>
-    </div>
+    <ul className='flex flex-col gap-2'>
+        {results.map((game) => (
+            <Link 
+              to={game.genres && game.genres.length > 0 ? `/browse/${game.genres[0].slug}/${game.id}` : '#'}
+              key={game.id} 
+              className=' flex items-center gap-6 cursor-pointer hover:bg-neutral-700 py-2 px-2 rounded-md'
+            >
+                <img src={game.background_image} alt='image' className='w-[60px] h-[40px] rounded-md'/>
+                <h3 className='text-neutral-200'>{game.name}</h3>
+            </Link>
+        ))}
+    </ul>
   )
 }
