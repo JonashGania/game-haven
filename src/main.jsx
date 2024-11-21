2de7b76ef8d24a4087e7f013c910cf8b95eec5ac
JonashGana <jonashtiglao@gmail.com>
2024-11-21 22:17:17 +0800

    created Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { WishlistProvider } from './context/WishlistContext.jsx'
-import App from './App.jsx'
 import './index.css'
+import { WishlistProvider } from './context/WishlistContext.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './pages/Home/Home.jsx'
+import ErrorPage from './components/ErrorPage.jsx'
+import Browse from './pages/Browse/Browse.jsx'
+import GameDetailsPage from './pages/GameDetails/GameDetails.jsx'
+import { SkeletonTheme } from 'react-loading-skeleton'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/browse',
+    element: <Browse />,
+  },
+  {
+    path: '/browse/:genreName/:gameSlug',
+    element: <GameDetailsPage />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <WishlistProvider >
-        <App />
+      <WishlistProvider>
+        <SkeletonTheme baseColor='#313131' highlightColor='#525252'>
+          <RouterProvider router={router}/>
+        </SkeletonTheme>
       </WishlistProvider>
-    </BrowserRouter>
   </React.StrictMode>,
 )
