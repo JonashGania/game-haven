8d3ecaa64887b1b4103035c8585c7326459db07c
JonashGana <jonashtiglao@gmail.com>
2024-11-21 22:16:46 +0800

    added genre list data

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,4 +1,64 @@
 export const data = {
+    genres: [
+        {
+            genre: 'Action',
+            slug: 'action'
+        },{   
+            genre: 'Indie',
+            slug: 'indie'
+        }, {   
+            genre: 'Adventure',
+            slug: 'adventure'
+        }, {
+            genre: 'RPG',
+            slug: 'role-playing-games-rpg',
+        }, {
+            genre: 'Strategy',
+            slug: 'strategy',
+        }, {
+            genre: 'Shooter',
+            slug: 'shooter',
+        }, {
+            genre: 'Casual',
+            slug: 'casual',
+        }, {
+            genre: 'Simulation',
+            slug: 'simulation',
+        }, {
+            genre: 'Puzzle',
+            slug: 'puzzle',
+        }, {
+            genre: 'Arcade',
+            slug: 'arcade'
+        }, {
+            genre: 'Platformer',
+            slug: 'platformer'
+        }, {
+            genre: 'Racing',
+            slug: 'racing'
+        }, {
+            genre: 'Massively Multiplayer',
+            slug: 'massively-multiplayer'
+        }, {
+            genre: 'Sports',
+            slug: 'sports'
+        }, {
+            genre: 'Fighting',
+            slug: 'fighting'
+        }, {
+            genre: 'Family',
+            slug: 'family'
+        }, {
+            genre: 'Board Games',
+            slug: 'board-games'
+        }, {
+            genre: 'Educational',
+            slug: 'educational'
+        }, {
+            genre: 'Card',
+            slug: 'card'
+        }
+    ],
     usefulLink: [
         {
             page: 'My Github',
