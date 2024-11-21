6e1cac398754be5acc862b8d6843a6de34b8b89b
JonashGana <jonashtiglao@gmail.com>
2024-11-21 22:14:47 +0800

    refactor code

--- src/api/api.js
+++ src/api/api.js
@@ -1,23 +1,14 @@
 import axios from "axios";
 import { formattedDate } from "../utils/formattedDate";
 
-export async function getGenreList(){
-    try{
-        const response = await axios.get(`https://api.rawg.io/api/genres?key=${import.meta.env.VITE_API_KEY}`);
-        const genreList = response.data.results;
-
-        return { genreList }
-    } catch(error){
-        console.error("Error fetching data:", error);
-    }
-}
+const BASE_URL = 'https://api.rawg.io/api'
 
 export async function getGamesList(genre){
     try{
-        const response = await axios.get(`https://api.rawg.io/api/games?key=${import.meta.env.VITE_API_KEY}&genres=${genre}`);
+        const response = await axios.get(`https://api.rawg.io/api/games?&genres=${genre}&key=${import.meta.env.VITE_API_KEY}`);
         const genreGames = response.data.results;
 
-        return {genreGames}
+        return genreGames
     } catch (error) {
         console.error("Error fetching data:", error)
     }
@@ -37,60 +28,17 @@ export async function getNewestGames(){
     }
 }
 
-export async function getSteamTopGames() {
-    try{
-        const steam = [];
-        const response = await axios.get(`https://api.rawg.io/api/games?key=${import.meta.env.VITE_API_KEY}`);
-        
-        const topGames = response.data.results;
 
-        topGames.forEach((game) => {
-            const stores = game.stores.map((store) => store.store.name);
+export const getStoreTopGames = async (storeId, pageSize) => {
+    const response = await axios.get(`${BASE_URL}/games?store=${storeId}&page_size=${pageSize}&key=${import.meta.env.VITE_API_KEY}`);
+    const data = response.data;
+    return data.results;
+} 
 
-            if (stores.includes('Steam')){
-                steam.push(game);
-            }  
-        })
-        return { steam, topGames }
-    } catch (error) {
-        console.error("Error fetching data:", error)
-    }
-}
-
-
-export async function getStoreTopGames(){
+export async function getGameDetails(gameSlug) {
     try {
-        const playstation = [];
-        const xbox = [];
-
-        const { formattedCurrentDate, formattedFiveYearsAgo } = formattedDate();
-        const dateRange = `${formattedFiveYearsAgo},${formattedCurrentDate}`;
-
-        const response = await axios.get(`https://api.rawg.io/api/games?key=${import.meta.env.VITE_API_KEY}&dates=${dateRange}`);
-        const topPlaystationGames = response.data.results;
-
-        topPlaystationGames.forEach((game) => {
-            const stores = game.stores.map((store) => store.store.name);
-
-            if(stores.includes('PlayStation Store')){
-                playstation.push(game)
-            }
-
-            if(stores.includes('Xbox Store')){
-                xbox.push(game)
-            }
-        })
-
-        return { playstation, xbox }
-    } catch (error) {
-        console.error("Error fetching data:", error)
-    }
-}
-
-export async function getGameDetails(gameId) {
-    try {
-        const response = await axios.get(`https://api.rawg.io/api/games/${gameId}?key=${import.meta.env.VITE_API_KEY}`)
-        const fetch = await axios.get(`https://api.rawg.io/api/games/${gameId}/screenshots?key=${import.meta.env.VITE_API_KEY}`)
+        const response = await axios.get(`https://api.rawg.io/api/games/${gameSlug}?key=${import.meta.env.VITE_API_KEY}`)
+        const fetch = await axios.get(`https://api.rawg.io/api/games/${gameSlug}/screenshots?key=${import.meta.env.VITE_API_KEY}`)
         const results = response.data;
         const screenShots = fetch.data.results;
 
