63715a7e4ee244278150d042e1531057a452533a
JonashGana <jonashtiglao@gmail.com>
2024-11-21 22:15:07 +0800

    refactor code

--- src/components/FilterGenre.jsx
+++ src/components/FilterGenre.jsx
@@ -1,73 +1,11 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { CgMenuGridO } from "react-icons/cg";
 import { IoMdClose } from "react-icons/io";
-import { getGenreList } from '../api/api';
-import { useNavigate } from 'react-router-dom';
-import SkeletonFilterGenre from './Skeleton/SkeletonFilterGenre';
-import PropTypes from 'prop-types';
+import { data } from '../constants/constant';
 
-
-function Genre({ handleClose }){
-    const [genres, setGenre] = useState([])
-    const [isLoading, setIsLoading] = useState(true);
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        async function fetchGenres(){
-          try {
-            setIsLoading(true);
-            const { genreList } = await getGenreList();
-            setGenre(genreList);
-            setIsLoading(false);
-          } catch (error) {
-            console.error('Error fetching data', error)
-          }
-        }
-    
-        fetchGenres();
-    }, [])
-
-    const handleGenreNavigate = (genreName) => {
-        navigate(`/browse/${genreName}`);
-        handleClose();
-      }
-
-
-    return (
-        <div className='bg-[rgb(34,34,34)] fixed top-0 left-0 w-full h-screen py-4 px-4 z-30 cursor-default'>
-            <div className='flex justify-between pb-4'>
-                <h3 className='text-neutral-200 text-lg font-semibold'>Genre</h3>
-                <button onClick={handleClose}>
-                    <IoMdClose 
-                        size='1.5rem' 
-                        color='white'
-                    />
-                </button>
-            </div>
-            <ul className='grid grid-cols-2 gap-3 gap-y-5'>
-                {isLoading ? (
-                    Array.from({ length: 19 }).map((_, index) => (
-                        <SkeletonFilterGenre key={index}/>
-                    ))
-                ) : (
-                    genres.map((genre) => (
-                        <li
-                            key={genre.id} 
-                            className='flex items-center gap-3 cursor-pointer'
-                            onClick={() => handleGenreNavigate(genre.slug)}
-                        >
-                            <img src={genre.image_background} alt='genre image' className='w-[60px] h-[40px] rounded-lg'/>
-                            <span className='text-neutral-300 text-xs mobile:text-sm'>{genre.name}</span>
-                        </li>
-                    ))
-                )}
-            </ul>
-        </div>
-    )
-}
-
-export default function FilterGenre() {
+export default function FilterGenre({ onClickGenre }) {
     const [isOpen, setIsOpen] = useState(false);
+    const genres = data.genres
 
     const handleOpen = () => {
         setIsOpen(true);
@@ -77,8 +15,13 @@ export default function FilterGenre() {
         setIsOpen(false);
     }
 
+    const handleGenreSelect = (genre) => {
+        onClickGenre(genre);
+        setIsOpen(false);
+    }
+
     return (
-        <div className='relative'>
+        <div className='relative block sidebar:hidden pb-4'>
             <button
                 className='flex items-center gap-2 relative cursor-pointer' 
                 onClick={handleOpen}
@@ -89,12 +32,31 @@ export default function FilterGenre() {
                     color='white'
                 />
             </button>
-            {isOpen && <Genre handleClose={handleClose}/>}
+            {isOpen && (
+                <div className='bg-[rgb(34,34,34)] fixed top-0 left-0 w-full h-screen py-4 px-4 z-30 cursor-default'>
+                    <div className='flex justify-between pb-4'>
+                        <h3 className='text-neutral-200 text-lg font-semibold'>Genre</h3>
+                        <button onClick={handleClose}>
+                            <IoMdClose 
+                                size='1.5rem' 
+                                color='white'
+                            />
+                        </button>
+                    </div>
+                    <ul className='grid grid-cols-2 gap-3 gap-y-5'>
+                        {genres.map((genre, index) => (
+                            <li
+                                key={index} 
+                                className='flex items-center gap-3 cursor-pointer'
+                                onClick={() => handleGenreSelect(genre.slug)}
+                            >
+                                <span className='text-neutral-300 text-xs mobile:text-sm'>{genre.genre}</span>
+                            </li>
+                        ))
+                        }
+                    </ul>
+                </div>
+            )}
         </div>
     )
-}
-
-
-Genre.propTypes = {
-    handleClose: PropTypes.func.isRequired,
 }
