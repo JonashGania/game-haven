f69fa9f48440d19756ba06799d4bf8ee67480579
JonashGana <jonashtiglao@gmail.com>
2024-11-22 22:58:57 +0800

    used tanstack query in fetching data

--- src/pages/Browse/Browse.jsx
+++ src/pages/Browse/Browse.jsx
@@ -1,37 +1,22 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import { fetchGames } from '../../api/api'
+import { useQuery } from '@tanstack/react-query'
 import Header from '../../components/Header'
 import Sidebar from '../../components/Sidebar'
 import GameLists from './Games/GameLists'
 import FilterGenre from '../../components/FilterGenre'
 import WishList from '../../components/WishList'
-import { getStoreTopGames, getGamesList } from '../../api/api'
 
 export default function Browse() {
-  const [gamesLists, setGamesLists] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
   const [selectedGenre, setSelectedGenre] = useState(null);
 
-  const loadCountries = async (genre) => {
-    try {
-      const games = genre
-        ? await getGamesList(genre)
-        : await getStoreTopGames(1, 20)
-
-        setGamesLists(games);
-        setSelectedGenre(genre);
-    } catch (error) {
-      console.error('Failed to fetch games', error)
-    } finally {
-      setIsLoading(false);
-    }
-  } 
-
-  useEffect(() => {
-    loadCountries(null);
-  }, [])
+  const {data, isLoading, error} = useQuery({
+    queryKey: ['browseGames', selectedGenre],
+    queryFn: () => fetchGames(selectedGenre),
+  })
 
   const handleClickGenre = (genre) => {
-    loadCountries(genre);
+    setSelectedGenre(genre);
   }
 
   return (
@@ -41,7 +26,7 @@ export default function Browse() {
         <Sidebar onClickGenre={handleClickGenre}/>
         <div className='w-full'>
           <FilterGenre onClickGenre={handleClickGenre}/>
-          <GameLists gameLists={gamesLists} selectedGenre={selectedGenre} isLoading={isLoading}/>
+          <GameLists gameLists={data} selectedGenre={selectedGenre} isLoading={isLoading}/>
         </div>
       </div>
       <WishList />
