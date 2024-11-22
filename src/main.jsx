c985bd458db710411b75e831e7664562a4555003
JonashGana <jonashtiglao@gmail.com>
2024-11-22 22:58:23 +0800

    setup QueryClient

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { WishlistProvider } from './context/WishlistContext.jsx'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { SkeletonTheme } from 'react-loading-skeleton'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Home from './pages/Home/Home.jsx'
 import ErrorPage from './components/ErrorPage.jsx'
 import Browse from './pages/Browse/Browse.jsx'
 import GameDetailsPage from './pages/GameDetails/GameDetails.jsx'
-import { SkeletonTheme } from 'react-loading-skeleton'
 
 const router = createBrowserRouter([
   {
@@ -25,12 +26,24 @@ const router = createBrowserRouter([
   }
 ])
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 300000,
+      gcTime: 10 * 60 * 1000,
+      refetchOnWindowFocus: false,
+    }
+  }
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <WishlistProvider>
-        <SkeletonTheme baseColor='#313131' highlightColor='#525252'>
-          <RouterProvider router={router}/>
-        </SkeletonTheme>
-      </WishlistProvider>
+      <QueryClientProvider client={queryClient}>
+        <WishlistProvider>
+          <SkeletonTheme baseColor='#313131' highlightColor='#525252'>
+            <RouterProvider router={router}/>
+          </SkeletonTheme>
+        </WishlistProvider>
+      </QueryClientProvider>
   </React.StrictMode>,
 )
